fa196f4fa6b58fa44a55ffd98f92c25d4ff7897b
chaiyarerk-hom <chaiyarerk.hom@zohomail.com>
2025-10-08 05:52:53 -0400

    Python lint 4

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -111,13 +111,12 @@ def initiate():
             "year": 2023,
             "car_make": car_make_instances[4]
         },
-            # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
-            CarModel.objects.create(
-                name=data['name'],
-                car_make=data['car_make'],
-                type=data['type'],
-                year=data['year']
-            )
+        CarModel.objects.create(
+            name=data['name'],
+            car_make=data['car_make'],
+            type=data['type'],
+            year=data['year']
+        )
